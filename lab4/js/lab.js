c1847c3f8ebf3cc51ecf1629fa2ab51388917777
tavluvsroses2 <64922695+tavluvsroses2@users.noreply.github.com>
2020-09-14 13:24:28 -0700

    Lab 4 Final Chamges

---
+++ lab4/js/lab.js
@@ -0,0 +1,13 @@
+//Print out 6 random cards backs facing.
+//Player chooses two cards.
+//If match.
+  //Then match is a win
+    //Give player points.
+    //Move cards to the side.
+    //Player chooses 2 cards
+//Else if no match
+  //Player repeats the process.
+  //No points given
+  //Player chooses two cards
+//If no cards are left
+  //Print “YOU WIN!” screen.
