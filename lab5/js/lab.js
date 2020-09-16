000ebbb2e8de5ee457c5e6faa963078335159f8d
tavluvsroses2 <64922695+tavluvsroses2@users.noreply.github.com>
2020-09-15 17:51:14 -0700

    Lab 5 Changes

---
+++ lab5/js/lab.js
@@ -0,0 +1,23 @@
+/*Author:Prisilla and Tavia <ptorres@csumb.edu> <teckhart@csumb.edu>
+  *Created: 15th of Spetember
+  *Lisence:Public Domain
+  */
+
+//Define Variables
+
+make = "Ford";
+model = "Focus";
+color = "Silver";
+year = 2016;
+
+
+//Calculate
+
+age = 2020 - year;
+
+//Output:
+document.writeln("Make: " + make + "<br>");
+document.writeln("Model: " + model + "<br>");
+document.writeln("Color: " + color + "<br>");
+document.writeln("Year: " + year + "<br>");
+document.writeln("Age: " + age + "<br>");
