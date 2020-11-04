9630417a11a0cf3d07b2e88228fd6b46694151fe
tavluvsroses2 <64922695+tavluvsroses2@users.noreply.github.com>
2020-11-03 19:45:01 -0800

    lab 17 and home page update

---
+++ lab16/js/lab16.js
@@ -0,0 +1,14 @@
+/*Author: Prisilla Torres and Tavia Eckhart
+Special thanks to Wes's Lecture Powerpoint
+Created: November 2nd
+License: Public Domain*/
+
+/*
+Using jQuery, do the following:
+*/
+
+//Add a click event to it that will bring up an alert
+$("#btn1").click(function(){
+	$("#btn1").toggleClass("green");
+})
+//Restyle the button with jQuery so it is a pleasant green.
