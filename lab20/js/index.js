55f6138045ec0133529b5d48d85c41da364592a2
Tavia Eckhart <64922695+tavluvsroses2@users.noreply.github.com>
2020-11-19 19:29:32 -0800

    labs and accordian

---
+++ lab20/js/index.js
@@ -0,0 +1,62 @@
+/*
+* Author: Tavia and Prisilla
+* Created: 11 November 2020
+* License: Public Domain
+* Credit: Wes Modes via thier GitHub cst 252 repository
+*/
+
+
+var URL = "https://xkcd.com/info.0.json";
+
+var URLpre = "https://xkcd.com/";
+var URLpost = "info.0.json";
+
+// Endpoint format: http://xkcd.com/614/info.0.json
+
+function getComic(num) {
+  if (typeof num === 'undefined') {
+    numStr = "";
+  } else {
+    numStr = num.toString() + "/";
+  }
+  var ourURL = URLpre + numStr + URLpost;
+  // get data via ajax from numbersapi
+  // Using the core $.ajax() method
+  $.ajax({
+      // The URL for the request (ENDPOINT)
+      url: ourURL,
+      // Whether this is a POST or GET request
+      type: "GET",
+  })
+  // If the request succeeds
+  .done(function(data) {
+      // console.log(data);
+      var imageUrl = data.img;
+      var title = data.title;
+      var alt = data.alt;
+      var comicNum = data.num;
+      var html = `<div id="imageblock">
+          <h2>${title}</h2>
+          <img src="${imageUrl}" title="${alt}"><br>
+          <button id="prev">Previous</button><button id="next">Next</button>
+        </div>`
+      $("#output").html(html);
+
+      // add event listener to new prev button
+      $("#prev").click(function(){
+        getComic(comicNum - 1);
+      });
+      // add event listener to new next button
+      $("#next").click(function(){
+        getComic(comicNum + 1);
+      });
+  })
+  .fail(function(){
+    console.log("^^ Please ignore this error. It's okay.");
+    console.log("Have a happy day! :-)");
+  })
+
+}
+
+// start things off
+getComic();
