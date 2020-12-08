b3ae15b6feddaa4bf6634c302fa708eba285a2a9
tavluvsroses2 <64922695+tavluvsroses2@users.noreply.github.com>
2020-12-07 16:27:49 -0800

    Midterm Final Updates

--- midterm/js/lab.js
+++ midterm/js/lab.js
@@ -1,4 +1,4 @@
-/*Author: Tavia Eckhart, Sean Brown, Kevin Martelli
+/*Author: Tavia Eckhart, Sean Brown, Kevin Martelli, Sebastian Tamiano
 Special thanks to Wes's Lecture Powerpoint, https://codepen.io/P1N2O/pen/pyBNzX
 Created: October 2, 2020
 License: Public Domain*/
@@ -25,22 +25,42 @@ License: Public Domain*/
                    "I can't give away my secrets just yet.",
                    "Cannot predict now",
                    "Concentrate and check the console."];
+
 // When the page loads it initiates the function that calls each element by ID
-    window.onload = function() {
-       var eight = document.getElementById("eight");
-       var answer = document.getElementById("answer");
-       var eightball = document.getElementById("eight-ball");
-       var question = document.getElementById("question");
+
+       var eight = $("#eight");
+       var answer = $("#answer");
+       var eightball = $("#eight-ball");
+       var question = $("#question");
+
+
 // when you click the eightball element trigger a function that takes the length of the input string and checks to see if its less than 2 letters
-       eightball.addEventListener("click", function() {
-         if (question.value.length < 2) {
-           //less than two letters prompts the user to enter a question
-           alert('Enter a question!');
-         } else {
-           //change the eightball's text to a random answers
-           eight.innerText = "";
-           var num = Math.floor(Math.random() * Math.floor(answers.length));
-           answer.innerText = answers[num];
-         }
-       });
-    };
+
+eightball.click (displayAnswer);
+
+//The function that hashes a str into a number .works
+function hashCode (str){
+     var hash = 0;
+     if (str.length == 0) return hash;
+     for (i = 0; i < str.length; i++) {
+         char = str.charCodeAt(i);
+         hash = ((hash<<5)-hash)+char;
+         hash = hash & hash; // Convert to 32bit integer
+     }
+     return Math.abs(hash);
+};
+
+// funtion applies hashcode to the input hecks the input length
+ function displayAnswer (){
+   question = $("#question").val();
+    i = hashCode(question) % answers.length
+    console.log(answers[i]);
+    answer.html(answers[i]);
+  };
+    //   if (question.value.length < 2) {
+    //     //less than two letters prompts the user to enter a question
+    //     alert('Enter a question!');
+    //   } else {
+    //     //change the eightball's text to a random answers
+    //     eight.innerText = "";
+    //     var num = Math.floor(Math.random() * Math.floor(answers.length));
